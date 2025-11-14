b804440f8c5d59a2aaec03daf5cbfd7748d2b366
Rick MacTurk <rmacturk@eitccorp.com>
2025-11-14 15:49:20 -0600

    new invoice icon

--- js/main.js
+++ js/main.js
@@ -30,7 +30,13 @@ class Header extends HTMLElement {
                             <img src="images/site/pf_16.png" alt="Book with Practice Fusion" height="16" width="16" /><span class="pl-5">Book Appointment</span>
                         </button>
                         <button class="button-primary">
-                            <img src="images/site/invoice.svg" alt="Pay Invoice" height="16" width="16" /><span class="pl-5">Pay Invoice</span>
+                            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3 10C3 6.22876 3 4.34315 4.17157 3.17157C5.34315 2 7.22876 2 11 2H13C16.7712 2 18.6569 2 19.8284 3.17157C21 4.34315 21 6.22876 21 10V14C21 17.7712 21 19.6569 19.8284 20.8284C18.6569 22 16.7712 22 13 22H11C7.22876 22 5.34315 22 4.17157 20.8284C3 19.6569 3 17.7712 3 14V10Z" stroke="var(--highlight-text)" stroke-width="1.5"/>
+                                <path d="M8 12H16" stroke="var(--highlight-text)" stroke-width="1.5" stroke-linecap="round"/>
+                                <path d="M8 8H16" stroke="var(--highlight-text)" stroke-width="1.5" stroke-linecap="round"/>
+                                <path d="M8 16H13" stroke="var(--highlight-text)" stroke-width="1.5" stroke-linecap="round"/>
+                            </svg>
+                            <span class="pl-5">Pay Invoice</span>
                         </button>
                     </div>
                 </div>
